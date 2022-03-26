8a1a16c4b5f8027b379efb3a53c83f5db2021581
anastasiia-bgd <96425997+anastasiia-bgd@users.noreply.github.com>
2022-03-26 12:01:58 +0300

    save poke

---
+++ addRandom.js
@@ -0,0 +1,38 @@
+import { fillPokemonCard, getPokemonByNameOrId,} from './const.js';
+const btn = document.querySelector('#add');
+const cardList = document.querySelector('.card-list')
+
+btn.addEventListener('click', async (event) => {
+    const randomId = Math.floor(Math.random() * 100);
+    try {
+        const pokemon = await getPokemonByNameOrId(randomId);
+        fillPokemonCard(pokemon);
+        // const savePoke = JSON.stringify(cardList.innerHTML)
+       localStorage.setItem('save', cardList.innerHTML);
+    
+    } catch (error) {
+        alert(error.message);
+    }
+
+    // fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+    //     .then((response) => response.json())
+    //     .then((result) => {
+    //         const {
+    //             name,
+    //             base_experience: experience,
+    //             // abilities,
+    //             abilities: rawAbilities,
+    //             sprites: { front_default: image },
+    //             // sprites: { front_default },
+    //         } = result;
+    //         const serailizedAbilities = rawAbilities.map((el) => {
+    //             const {
+    //                 ability: { name },
+    //             } = el;
+    //             return name;
+    //         });
+    //         const pokemon = new Poke(name, serailizedAbilities, experience, image);
+    //         fillPokemonCard(pokemon);
+    //     });
+});
+cardList.innerHTML = localStorage.getItem('save')
